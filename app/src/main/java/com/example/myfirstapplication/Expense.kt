af0cd837f4d5f48531fe91260d9047429f82c764
“Manasvi <W0835535@myscc.ca>
2025-04-02 15:58:09 -0400

    initial commit

--- app/src/main/java/com/example/myfirstapplication/Expense.kt
+++ app/src/main/java/com/example/myfirstapplication/Expense.kt
@@ -2,4 +2,4 @@ package com.example.myfirstapplication
 
 import android.icu.util.Currency
 
-data class Expense(val name: String, val amount: Double, val date: String, val currency: Currency,val convertedCost: Double)
+data class Expense(val name: String, val amount: Double, val date: String, val currency: Currency,val convertedCost: Double = 0.0)
